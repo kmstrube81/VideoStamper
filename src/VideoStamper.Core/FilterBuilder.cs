97f5cd3ac7074d44c313c92b0c997e09505145a3
kmstrube81 <kasey@kmstrube.net>
2026-01-26 09:39:56 -0600

    Update FilterBuilder.cs

--- src/VideoStamper.Core/FilterBuilder.cs
+++ src/VideoStamper.Core/FilterBuilder.cs
@@ -7,6 +7,15 @@ namespace VideoStamper.Core;
 
 public static class FilterBuilder
 {
+	private static string EscapeFfmpegString(string input)
+	{
+		return input.Replace("'", "\\'")      // escape single quotes
+                                   .Replace("\\", "\\\\\\\\") // escape backslashes
+                                   .Replace(":", "\\\\\\:")   // escape colons for filter syntax
+                                   .Replace(",", "\\\\,")     // escape commas
+                                   .Replace("/", "\\\\/");    // escape forward slash
+	}
+	
     public static string BuildFilterComplexForInput(
         InputSettings input,
         VideoMetadata meta
@@ -170,11 +179,7 @@ public static class FilterBuilder
         string ffmpegFormat = ConvertTimeFormat(ts.Format);
 
         // 2. Support multi-line format strings
-        ffmpegFormat = ffmpegFormat.Replace("'", "\\'")      // escape single quotes
-                                   .Replace("\\", "\\\\\\\\") // escape backslashes
-                                   .Replace(":", "\\\\\\:")   // escape colons for filter syntax
-                                   .Replace(",", "\\\\,")     // escape commas
-                                   .Replace("/", "\\\\/");    // escape forward slash
+        ffmpegFormat = EscapeFfmpegString(ffmpegFormat);
 
         string[] newlines = ffmpegFormat.Split(
             Environment.NewLine,
@@ -215,7 +220,7 @@ public static class FilterBuilder
 
             DrawText filter = new DrawText(
                 text,
-                ts.Font.FontFile,
+                EscapeFfmpegString(ts.Font.FontFile),
                 ts.Font.Size,
                 ts.Font.Color,
                 xExpr,
@@ -293,7 +298,7 @@ public static class FilterBuilder
                 oad = sub.AnimationOutDur;
             }
 
-            DrawText filter = new DrawText(newtext, sub.Font.FontFile, sub.Font.Size, sub.Font.Color, xExpr, yExpr, bc, bw, start, end, ia, iad, oa, oad);
+            DrawText filter = new DrawText(newtext, EscapeFfmpegString(sub.Font.FontFile), sub.Font.Size, sub.Font.Color, xExpr, yExpr, bc, bw, start, end, ia, iad, oa, oad);
             filter.XCoord = xc;
             filter.YCoord = yc;
 
